217322adc78fae2a9b524a8fd831d0ec59c20f32
Sateesh Gundeboina <sateesh.gundeboina@livingstone-tech.com>
2025-06-03 11:49:15 +0530

    push updates

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -62,7 +62,6 @@ var app = builder.Build();
 // Run migrations during startup
 using (var scope = app.Services.CreateScope())
 {
-    Console.WriteLine("Done");
     var dbContext = scope.ServiceProvider.GetRequiredService<UserDbContext>();
     dbContext.Database.Migrate();
 }
